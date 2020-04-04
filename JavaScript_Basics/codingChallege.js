683e869d8b355573456bd82517e8942e504f140c
AndreyMora04 <moraandrey1@gmail.com>
2020-04-04 16:49:40 -0600

    Added of a new Js Class to code a challenge.

---
+++ JavaScript_Basics/codingChallege.js
@@ -0,0 +1,20 @@
+//Basic Operators and first coding challenge:
+
+var maxHeight = 1.90;
+var maxMass = 90;
+var maxBMI = (maxMass) / (maxHeight * maxHeight);
+
+
+var jonhHeight = 1.80;
+var jonhMass = 80;
+var jonhBMI = (jonhMass) / (jonhHeight * jonhHeight);
+
+if(maxBMI > jonhBMI){
+
+alert('Is Max BMI hihger than Jonh?' + ' ' + true)
+
+}else{
+
+alert('Sorry :c')
+
+}
