cceeab5d190a82ebf4b09284c527bb1f5b504957
AndreyMora04 <moraandrey1@gmail.com>
2020-04-04 16:51:45 -0600

    Knowing events function on JavaScript.

---
+++ JavaScript_Basics/script.js
@@ -0,0 +1,41 @@
+//Data types, printing our data and using basic functions.
+//For convention, we always use cammel case variable naming.
+
+var firstName = 'Jonh';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job)
+
+/*
+
+Rules for naming a variable:
+
+1.We can't use numbers to start a variable.
+2.We can't use special character to start a variable.
+3.We can't use reserve words of the languaje to write a variable.
+
+*/
+
+//Using events in the browser, prompta and alert, these are events to show data.
+
+console.log('His first name is:' + ' ' + firstName + ' '+'and he is:' + ' ' + age);
+
+var age = prompt('Jonh is on his birthday, which is his actual age now?')
+var firstName = prompt('Which is John Surname?')
+
+console.log('Updated data of Jonh is:')
+console.log('His first name is:' + ' ' + firstName + ' '+'and he is:' + ' ' + age);
+
+
+
+
